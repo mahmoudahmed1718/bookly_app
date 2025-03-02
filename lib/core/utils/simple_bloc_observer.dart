2897363ba031cddbad27cc510507c49066067798
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-03-02 15:35:39 +0200

    create simple bloc observer

---
+++ lib/core/utils/simple_bloc_observer.dart
@@ -0,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log(change.toString());
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/functions/setup_server_locator.dart';
 
 import 'package:bookly_app/core/utils/app_routers.dart';
+import 'package:bookly_app/core/utils/simple_bloc_observer.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo_imp.dart';
 import 'package:bookly_app/features/home/domain/uses_case/fetch_featured_book_use_case.dart';
 import 'package:bookly_app/features/home/presentation/manger/cubit/fetch_featured_books_cubit.dart';
@@ -19,6 +20,7 @@ void main() async {
   setupServerLocator();
   await Hive.openBox(kFeatuedBox);
   await Hive.openBox(kNewbox);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const Bookly());
 }
 
